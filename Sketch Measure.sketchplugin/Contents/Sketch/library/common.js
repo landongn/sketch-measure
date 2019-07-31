62597974cc285897b7d27e3add7a02ab19071fff
Landon Noss <landongn@gmail.com>
2019-07-30 17:34:20 -0700

    uhhhh

--- Sketch Measure.sketchplugin/Contents/Sketch/library/common.js
+++ Sketch Measure.sketchplugin/Contents/Sketch/library/common.js
@@ -16,16 +16,45 @@ function _(str, data){
         return data[idx];
     });
 }
+const sketch = require('sketch')
+const pages = sketch.getSelectedDocument().pages
+const doc = sketch.getSelectedDocument()
+
+//Search in document pages for symbols
+var inspectSelection = function(layers) {
+    layers.forEach(function(layers){
+        if(layers.layers != undefined){
+            inspectSelection(layers.layers)
+        }
+        if(layers.layers == undefined){
+            if(layers.type == 'SymbolInstance'){
+                var group = layers.detach({
+                    recursively: true,
+                })
+            }
+        }
+    })
+}
+
+//Sends all document layers to detach
+var detachDocumentSymbols = function(){
+    pages.forEach(function(page) {
+        inspectSelection(page.layers)
+    })
+    sketch.UI.message(`🎉 All document symbols have been detached 🎉 `)
+}
+
+//Sends all selected layers to detach
+var detachSelectedSymbols = function(){
+    inspectSelection(doc.selectedLayers.layers)
+    sketch.UI.message(`🎉 Selected symbols have been detached 🎉 `)
+}
 
 var SM = {
         init: function(context, command){
             Sketch = new API();
             ga = new Analytics(context);
-            var documentData = context.document.documentData();
-            for (var i = 0; i < documentData.foreignSymbols().count(); i++) {
-                var foreignSymbol = documentData.foreignSymbols().objectAtIndex(i);
-                foreignSymbol.unlinkFromRemote();
-            }
+           
 
             this.prefs = NSUserDefaults.standardUserDefaults();
             this.context = context;
@@ -123,6 +152,7 @@ var SM = {
                         this.settingsPanel();
                         break;
                     case "export":
+                        detachDocumentSymbols()
                         this.export();
                         break;
                 }
